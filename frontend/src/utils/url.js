30ff34754bd4e6b4c6013b1234a0f43260c4e61b
RiyaShah553 <shahriya553@gmail.com>
2024-06-29 02:05:43 -0800

    Update url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,3 +1,3 @@
 
-const BASE_URL = 'https://expense-tracker-api-silk-kappa.vercel.app/api/v1';
+const BASE_URL = 'https://expense-tracker-api-silk-kappa.vercel.app/api/v1/';
 export default BASE_URL;
